3610580808f7bab9b54895afb0fbbe86510efdf8
sumon paul <sumpaul100@gmail.com>
2023-01-15 19:16:30 +0600

    video and videos comopenent created

---
+++ src/components/Videos.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classes from "../Styels/videos.module.css";
+import Video from "./Video";
+
+const Videos = () => {
+  return (
+    <div className={classes.videos}>
+      <Video />
+    </div>
+  );
+};
+
+export default Videos;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Videos from "../Videos";
 
 const Home = () => {
-  return <div>This is home page</div>;
+  return (
+    <div>
+      <Videos />
+    </div>
+  );
 };
 
 export default Home;
