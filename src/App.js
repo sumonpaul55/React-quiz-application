8d924ef274f86df1edf503540d116399d53a5733
sumon paul <sumpaul100@gmail.com>
2023-01-17 19:34:49 +0600

    router install and set

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../src/Styels/App.css";
 import Layout from "./components/Layout";
-// import Login from "./components/pages/Login";
-// import Quiz from "./components/pages/Quiz";
+import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
+import Quiz from "./components/pages/Quiz";
 import Result from "./components/pages/Result";
-// import Home from "./components/pages/Home";
-// import Signup from "./components/pages/Signup";
+import Signup from "./components/pages/Signup";
 const App = () => {
   return (
     <>
-      <Layout>
-        {/* <Home /> */}
-        {/* <Signup /> */}
-        {/* <Login /> */}
-        {/* <Quiz /> */}
-        <Result />
-      </Layout>
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/quiz" element={<Quiz />} />
+            <Route path="/result" element={<Result />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </>
   );
 };
