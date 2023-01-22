e61714dd5ae68cf3c5225b4d9aea267e2e2cea02
sumon paul <sumpaul100@gmail.com>
2023-01-22 23:08:13 +0600

    call data from firebase using get function and give paremiter query. then its call data according to query

--- src/Hooks/useVideoList.js
+++ src/Hooks/useVideoList.js
@@ -1,13 +1,26 @@
-import { getDatabase, orderByKey, query, ref } from "firebase/database";
-import { useEffect } from "react";
+import { get, getDatabase, orderByKey, query, ref } from "firebase/database";
+import { useEffect, useState } from "react";
 
 const useVideoList = () => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   useEffect(() => {
     async function fetchVideo() {
       //database Related works
       const db = getDatabase();
       const videoRef = ref(db, "videos");
       const videoquery = query(videoRef, orderByKey());
+
+      try {
+        setError(false);
+        setLoading(true);
+        // Required firebase database
+        const snapshot = await get(videoquery);
+        setLoading(false);
+      } catch (err) {
+        console.log(err);
+      }
     }
     fetchVideo();
   }, []);
