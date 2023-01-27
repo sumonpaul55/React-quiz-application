8acd6f7f2d0b388fed75a8d3bb5df0a543dd85a4
sumon paul <sumpaul100@gmail.com>
2023-01-27 14:10:50 +0600

    video is comming and infintScrolling

--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,20 +1,28 @@
 /* eslint-disable array-callback-return */
-import React from "react";
+import React, { useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import { Link } from "react-router-dom";
 import useVideoList from "../Hooks/useVideoList";
-import classes from "../Styels/videos.module.css";
 import Video from "./Video";
 
 const Videos = () => {
-  const { loading, error, videos } = useVideoList(0);
+  const [page, setPage] = useState(10);
+  const { loading, error, videos } = useVideoList(page);
   return (
-    <div className={classes.videos}>
-      {videos.length > 0 &&
-        videos.map((video, index) => (
-          <Link to="quiz" key={index}>
-            <Video title={video.title} id={video.youtubeID} noq={video.noq} />
-          </Link>
-        ))}
+    <div>
+      {videos.length > 0 && (
+        <InfiniteScroll dataLength={videos.length} hasMore={true} next={() => setPage(page + 5)}>
+          {videos.map((video) =>
+            video.noq > 0 ? (
+              <Link to="quiz" key={video.youtubeID}>
+                <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+              </Link>
+            ) : (
+              <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+            )
+          )}
+        </InfiniteScroll>
+      )}
       {!loading && videos.length === 0 && <h2>Data not found</h2>}
       {error && <h2>There was an error</h2>}
       {loading && <h2>Data Loading...</h2>}
