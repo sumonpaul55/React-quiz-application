1fc7445edb6d1d1cb158b18f007bfaef804f8fe4
sumon paul <sumpaul100@gmail.com>
2023-02-11 20:43:22 +0600

    miniplayer is working

--- src/components/Miniplayer.js
+++ src/components/Miniplayer.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import playerImg from "../assets/images/3.jpg";
 import classes from "../Styels/miniplayer.module.css";
 
 const Miniplayer = () => {
+  const [status, setStatus] = useState(false);
+  const buttonRef = useRef();
+  function toggleminiPlayer() {
+    if (!status) {
+      buttonRef.current.classList.remove(classes.floatingBtn);
+      setStatus(true);
+    } else {
+      buttonRef.current.classList.add(classes.floatingBtn);
+      setStatus(false);
+    }
+  }
   return (
-    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`}>
+    <div className={`${classes.miniPlayer} ${classes.floatingBtn}`} ref={buttonRef} onClick={toggleminiPlayer}>
       <span className={`material-icons-outlined ${classes.open}`}> play_circle_filled </span>
-      <span className={`material-icons-outlined ${classes.close}`}> close </span>
+      <span className={`material-icons-outlined ${classes.close}`} onClick={toggleminiPlayer}>
+        {" "}
+        close{" "}
+      </span>
       <img src={playerImg} alt="" />
       <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
     </div>
